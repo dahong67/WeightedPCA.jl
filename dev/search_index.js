66d483a6eb9265d350867cd3e8240baeab019465
Documenter.jl <documenter@juliadocs.github.io>
2022-10-04 23:48:35 +0000

    build based on 1b54dcd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = WeightedPCA","category":"page"},{"location":"#WeightedPCA","page":"Home","title":"WeightedPCA","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for WeightedPCA.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [WeightedPCA]","category":"page"}]
+}
