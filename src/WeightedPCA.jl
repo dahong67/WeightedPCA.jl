85c92f0d82d6c496d8a8c5035aeed8a9d7414bc4
David Hong <dahong67@wharton.upenn.edu>
2022-10-07 18:02:53 -0700

    Add module docstring

--- src/WeightedPCA.jl
+++ src/WeightedPCA.jl
@@ -1,3 +1,4 @@
+"Weighted PCA module. Provides weighted principal component analysis (PCA) for data with samples of heterogeneous quality (heteroscedastic noise)."
 module WeightedPCA
 
 # Write your package code here.
